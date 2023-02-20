a5212635b48b712b63a5160299a4954718bda17e
Ron Hughes <rondonsonmusic@gmail.com>
2023-02-19 23:13:32 -0500

    completed exercise 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let rStr = string
 
+    if (num === 0) {
+        return ""
+    }
+    else if (num < 0) {
+        return 'ERROR'
+    }
+    else {
+    for (let i = 1; i < num; i++) {
+         rStr += string
+    }
+}
+    return rStr
 };
 
 // Do not edit below this line
